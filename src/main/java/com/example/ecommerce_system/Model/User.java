330bfc25abf42227a948f3d03dd159b2319237ae
AHMEDALHAMAMI <146365804+AHMEDALHAMAMI@users.noreply.github.com>
2026-09-19 04:48:31 +0100

    Refactor User model validation annotations

--- src/main/java/com/example/ecommerce_system/Model/User.java
+++ src/main/java/com/example/ecommerce_system/Model/User.java
@@ -1,48 +1,39 @@
-
 package com.example.ecommerce_system.Model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class User {
 
-    @NotEmpty(message = "Id must not be empty")
+    @NotBlank(message = "ID cannot be empty")
     private String id;
 
-    @NotEmpty(message = "Username must not be empty")
-    @Size(min = 6, message = "Username must be more than 5 characters")
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 4, message = "Username must be at least 4 characters")
     private String username;
 
-    @NotEmpty(message = "Password must not be empty")
-    @Size(min = 7, message = "Password must be more than 6 characters")
-    @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
-            message = "Password must contain letters and digits"
-    )
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
-    @NotEmpty(message = "Email must not be empty")
+    @NotBlank(message = "Email cannot be empty")
     @Email(message = "Email must be valid")
     private String email;
 
-    @NotEmpty(message = "Role must not be empty")
-    @Pattern(
-            regexp = "^(Admin|Customer)$",
-            message = "Role must be Admin or Customer"
-    )
+    @NotBlank(message = "Role cannot be empty")
+    @Pattern(regexp = "Customer|Admin", message = "Role must be Customer or Admin")
     private String role;
 
-    @NotNull(message = "Balance must not be empty")
-    @Positive(message = "Balance must be positive")
-    private Double balance;
+    @PositiveOrZero(message = "Balance cannot be negative")
+    private double balance;
+
+    private int purchaseCount = 0;
+
+    private String referredBy;
+
+    public User() {
+    }
 }
